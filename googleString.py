8b33006db1c0bcec39916813a81eac514af70ef8
Abhinav <abhinavdhere@gmail.com>
2015-04-21 18:21:38 +0530

    added comments

--- googleString.py
+++ googleString.py
@@ -1,13 +1,21 @@
-fPath="/home/abhinav/GoogleString"
-wPath="/home/abhinav/temp"
+#Code to convert given string into form appropriate for google search
+#Author: Abhinav Dhere
+fPath="/home/abhinav/GoogleString" #read path
+wPath="/home/abhinav/temp"	   #write path
 def getString(fPath):
+    '''
+    Reads string from file
+    '''
     file=open(fPath,'r')
     inString=file.read()
     return inString
 
 def modString():
+    '''
+    Convert the string to appropriate form
+    '''
     inString=getString(fPath)
-    inString=inString[:-1]
+    inString=inString[:-1]#to remove newlines
     inStringL=inString.split()
     inString=inStringL[0]
     i=1
@@ -17,6 +25,6 @@ def modString():
     return inString
 
 inString= modString()
-wFile=open(wPath,'w')
+wFile=open(wPath,'w')#write to file
 wFile.write(inString)
 wFile.close()
