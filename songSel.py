1f503fb9b4e5910e4c86c043577bbd70b704a323
Abhinav <abhinavdhere@gmail.com>
2015-04-24 14:38:39 +0530

    Random song selector

---
+++ songSel.py
@@ -0,0 +1,26 @@
+#Selects a random song from a list of songs
+#To be used by the bash sript to select a random song
+#Author: Abhinav Dhere
+
+import random
+
+def getSongList():
+	songList=[]
+	inFile=open("/home/abhinav/songList",'r')
+	for line in inFile:
+		line=line[:-1]
+		songList.append(line)
+	return songList
+
+def selSong():
+	songList=getSongList()
+	song=random.choice(songList)
+	return song
+
+def giveSong():
+	song=selSong()
+	outFile=open("/home/abhinav/temp",'w')
+	outFile.write(song)
+	outFile.close()
+
+giveSong()
